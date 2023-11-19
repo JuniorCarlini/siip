f7e350a9165438dbb69378c040534e93863a3d02
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2023-11-19 14:06:27 -0400

    imagens

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from inicio.views import index
+from inicio.views import index, proprietarios
  
 urlpatterns = [
     path('',index),
+    path('proprietarios/',proprietarios, name = 'proprietarios'),
 ]
--- inicio/views.py
+++ inicio/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'inicio/index.html')
+
+def proprietarios(request):
+    return render(request, 'inicio/proprietarios.html')
