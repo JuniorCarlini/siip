5485ba3c9e7fcd4ef8f6584a353ad3da9e959e6c
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2023-11-19 15:38:02 -0400

    telas navegando normalmente

--- inicio/urls.py
+++ inicio/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from inicio.views import index, proprietarios
  
 urlpatterns = [
-    path('',index),
+    path('',index, name= 'index'),
     path('proprietarios/',proprietarios, name = 'proprietarios'),
 ]
