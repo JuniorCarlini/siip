569fbb9bd217a6acfa4d1e8c77346329b88703f7
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2023-11-22 00:16:07 -0400

    banco de dados

--- inicio/admin.py
+++ inicio/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Proprietario, Propriedade, Armadilha
 
-# Register your models here.
+@admin.register(Proprietario)
+class ProprietarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'numero_telefone', 'email')
+
+@admin.register(Propriedade)
+class PropriedadeAdmin(admin.ModelAdmin):
+    list_display = ('nome_propriedade', 'proprietario', 'endereco',)
+
+@admin.register(Armadilha)
+class ArmadilhaAdmin(admin.ModelAdmin):
+    list_display = ('identificador', 'propriedade', 'coordenadas_geograficas', 'horario_ultima_sincronizacao')
--- inicio/models.py
+++ inicio/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Proprietario(models.Model):
+    nome = models.CharField(max_length=100)
+    imagem = models.ImageField(upload_to='proprietarios/', null=True, blank=True)
+    numero_telefone = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.nome
+
+class Propriedade(models.Model):
+    proprietario = models.ForeignKey(Proprietario, on_delete=models.CASCADE, related_name='propriedades')
+    nome_propriedade = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=255)
+    def __str__(self):
+        return self.nome_propriedade
+
+class Armadilha(models.Model):
+    propriedade = models.ForeignKey(Propriedade, on_delete=models.CASCADE, related_name='armadilhas')
+    identificador = models.CharField(max_length=50)
+    coordenadas_geograficas = models.CharField(max_length=50)  # ou use um campo específico para coordenadas geográficas
+    horario_ultima_sincronizacao = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.identificador
