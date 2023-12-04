a697cb970a9e65a312c0095796cd1f693d8e079b
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2023-12-04 19:20:57 -0400

    Atualizar o models.py

--- inicio/models.py
+++ inicio/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Proprietario(models.Model):
     nome = models.CharField(max_length=100)
-    imagem = models.ImageField(upload_to='proprietarios/', null=True, blank=True)
+    imagem = models.ImageField(upload_to='proprietarios/proprietarios/', null=True, blank=True)
     numero_telefone = models.CharField(max_length=15)
     email = models.EmailField(unique=True)
     def __str__(self):
