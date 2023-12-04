2d4f35eee0022b5abf594598ee7c1d1eb0a8eef2
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2023-12-04 12:00:27 -0400

    tabrla funiconal proprietarios

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from inicio.views import index, proprietarios
- 
+
 urlpatterns = [
     path('',index, name= 'index'),
     path('proprietarios/',proprietarios, name = 'proprietarios'),
--- inicio/views.py
+++ inicio/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render , redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render, redirect
 from .forms import ProprietarioForm
+from .models import Proprietario
 
 def index(request):
     return render(request, 'inicio/index.html')
@@ -7,10 +9,31 @@ def index(request):
 def proprietarios(request):
     form = ProprietarioForm()
 
+    # Obtendo todos os proprietários do banco de dados
+    proprietarios_list = Proprietario.objects.all()
+
+    # Obtendo todos os proprietários do banco de dados
+    proprietarios_list = Proprietario.objects.all()
+
+        # Configurando a paginação
+    paginator = Paginator(proprietarios_list, 15)  # 30 itens por página
+    page = request.GET.get('page')
+
+    try:
+        proprietarios = paginator.page(page)
+    except PageNotAnInteger:
+        # Se a página não é um número inteiro, entrega a primeira página.
+        proprietarios = paginator.page(1)
+    except EmptyPage:
+        # Se a página está fora do intervalo (por exemplo, 9999), entrega a última página de resultados.
+        proprietarios = paginator.page(paginator.num_pages)
+
     if request.method == 'POST':
         form = ProprietarioForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect(request.path)
 
-    return render(request, 'inicio/proprietarios.html', {'form': form})
+    return render(request, 'inicio/proprietarios.html',{'form': form, 'proprietarios': proprietarios})
+
+ #   return render(request, 'inicio/proprietarios.html', {'form': form, 'proprietarios': proprietarios})
