aaa869286b81fbf4477b6d77ebe7b06102a550bd
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2023-12-04 23:52:47 -0400

    tabela funcionando

--- propriedades/views.py
+++ propriedades/views.py
@@ -1,14 +1,14 @@
-# views.py no seu novo app
-from django.shortcuts import render
-from inicio.models import Propriedade
+from django.shortcuts import render, get_object_or_404
+from inicio.models import Proprietario, Propriedade
 
 def propriedades(request, proprietario_id):
+    # Recupere o proprietário com base no ID fornecido ou retorne um erro 404 se não existir
+    proprietario = get_object_or_404(Proprietario, id=proprietario_id)
     
-    # Recupere as propriedades do proprietário do banco de dados
-    propriedades = Propriedade.objects.filter(proprietario_id=proprietario_id)
+    # Recupere as propriedades associadas a esse proprietário
+    propriedades = Propriedade.objects.filter(proprietario=proprietario)
     
-    # Passe as propriedades para o contexto do template
-    context = {'proprietario_id': proprietario_id, 'propriedades': propriedades}
+    # Passe o proprietário e suas propriedades para o contexto do template
+    context = {'proprietario': proprietario, 'propriedades': propriedades}
     
-    return render(request, 'propriedades/propriedades.html', {'proprietario_id': 1})
-
+    return render(request, 'propriedades/propriedades.html', context)
