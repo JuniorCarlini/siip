9004e30c7ecae761563197bd450defd6b6a04102
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2023-12-12 14:55:28 -0400

    configuração do modal

--- armadilhas/views.py
+++ armadilhas/views.py
@@ -1,9 +1,10 @@
-# armadilhas/views.py
 from django.shortcuts import render, get_object_or_404
 from inicio.models import Propriedade, Armadilha
 from armadilhas.forms import ArmadilhaForm
 
 def armadilhas(request, propriedade_id):
     propriedade = get_object_or_404(Propriedade, id=propriedade_id)
+
     armadilhas = propriedade.armadilhas.all()
+    
     return render(request, 'armadilhas/armadilhas.html', {'propriedade': propriedade, 'armadilhas': armadilhas})
