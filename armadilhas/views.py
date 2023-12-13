45954d855d44f188e5fc5051ff6033cb3ac4b0ef
Junior Carlini <116855041+JuniorCarlini@users.noreply.github.com>
2023-12-13 11:13:45 -0400

    cadastro de armadilhas concluido

--- armadilhas/views.py
+++ armadilhas/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from inicio.models import Propriedade, Armadilha
-from armadilhas.forms import ArmadilhaForm
+from armadilhas.forms import ArmadilhaForm  # Corrigir o nome do formulário
 
 def armadilhas(request, propriedade_id):
     propriedade = get_object_or_404(Propriedade, id=propriedade_id)
 
     armadilhas = propriedade.armadilhas.all()
-    
-    return render(request, 'armadilhas/armadilhas.html', {'propriedade': propriedade, 'armadilhas': armadilhas})
+
+    # Se o formulário de criação de armadilha for submetido
+    if request.method == 'POST':
+        form = ArmadilhaForm(request.POST)  # Corrigir o nome do formulário
+        if form.is_valid():
+            armadilha = form.save(commit=False)
+            armadilha.propriedade = propriedade
+            armadilha.save()
+
+            return redirect('armadilhas', propriedade_id=propriedade_id)
+    else:
+        form = ArmadilhaForm()  # Corrigir o nome do formulário
+
+    context = {'propriedade': propriedade, 'armadilhas': armadilhas, 'form': form}  # Corrigir o nome da variável
+
+    return render(request, 'armadilhas/armadilhas.html', context)
